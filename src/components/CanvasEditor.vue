4551e067dd9d2a31ca420754da092e2ad3f8c42e
Leo <leo.lu@orderplus.com>
2022-06-20 21:08:50 +0800

    图片渲染、拖拽

--- src/components/CanvasEditor.vue
+++ src/components/CanvasEditor.vue
@@ -6,13 +6,13 @@
       :style="{
         width,
         height,
+        padding,
       }"
     >
       <div
         class="canvas-editor__element__cell"
         v-for="(element, index) in cellsImg"
-        :key="element.id"
-        :data-index="index"
+        :key="`${element.id}_${index}}`"
         :class="[inDrag != '' && inDrag === index.toString() ? 'drop-in' : '']"
         :style="{
           height: cellsList[index].height + 'px',
@@ -21,20 +21,34 @@
           left: cellsList[index].x + 'px',
           borderWidth: element.src && element.src != '' ? '0px' : '2px',
         }"
-        @drop="dropAdd"
-        @dragenter="dragEnter"
-        @dragover="dragEnter"
-        @dragleave="dragLeave"
+        @drop="dropAdd($event, index)"
+        @dragover="dragEnter($event, index)"
+        @dragleave="dragLeave($event, index)"
       >
-        <img v-show="element.src != ''" :src="element.src" @dragstart="dragCacheChange" />
-        <span class="del-btn" @click="handleDel(element)"></span>
+        <div class="canvas-editor__element__cell__box">
+          <img
+            v-show="element.src != ''"
+            :src="element.src"
+            @mousedown="imgMoveStart($event, index)"
+            @mouseup="imgMoveEnd()"
+            @mousemove="imgMove($event, index)"
+            @mouseleave="imgMoveEnd()"
+            :style="{
+              height: element.height + 'px',
+              width: element.width + 'px',
+              top: element.y + 'px',
+              left: element.x + 'px',
+            }"
+          />
+          <span class="del-btn" @click="handleDel(index)"></span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, nextTick, onMounted, watch } from "vue";
+import { ref, onMounted, watch } from "vue";
 
 import {
   CanvasFactory,
@@ -50,7 +64,16 @@ type CellsOptionType = {
   img?: any;
 };
 
-type FileOption = { id: string; src: string; file: File };
+type FileOption = {
+  id: string;
+  src: string;
+  file: File;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  scale: "x" | "y";
+};
 
 type TypeObject = {
   [key: string]: any;
@@ -65,14 +88,15 @@ const props = defineProps({
 
 const width = ref("");
 const height = ref("");
+const padding = ref("");
 const template = ref(0);
 
 const cellsList = ref();
 const cellsImg = ref();
 
 const inDrag = ref("");
-let dropCache: FileOption | null = null;
-let dropFrom: string = "";
+let imgCache: FileOption | null = null;
+let clearIndexForImg: string = "";
 
 watch([props.options], function ([newOptions]) {
   console.log(newOptions, "配置更新");
@@ -85,17 +109,6 @@ watch([props.options], function ([newOptions]) {
   }
 });
 
-const setDropCache = function (data: FileOption) {
-  // 设置图片缓存
-  if (!data || !data.id || !data.file || !data.src) {
-    dropCache = null;
-    console.warn("拖入文件数据异常");
-    return;
-  }
-  dropCache = { ...data };
-};
-defineExpose({ setDropCache });
-
 onMounted(function () {
   // const canvasFactory = new CanvasFactory({
   //   id: "canvas-editor__canvas",
@@ -104,20 +117,26 @@ onMounted(function () {
   initCells();
 });
 
+const config = {
+  min_box_padding: 80, // editor__box 与 editor__element 的最小间距 (编辑框与外部的最小间距)
+  element_padding: 40, // editor__element 的 padding 在 options.padding = 100% 时的值
+  element_radius: 20, // editor__element 的 border-radius 在 options.radius = 100% 时的值
+  cell_margin: 40, // editor__cell 的 margin 在 options.margin = 100% 时的值
+};
+
 const resize = function () {
   let dom = document.querySelector(".canvas-editor__box");
   if (!dom) {
     throw new Error("canvas-editor dom error");
   }
-  const padding = 80; // 编辑框与外部的最小间距
   const { width: size_width, height: size_height } = props.options;
   const { scale_width, scale_height } = {
     scale_width: Math.max(
-      parseFloat((size_width / (dom.clientWidth - padding)).toFixed(2)),
+      parseFloat((size_width / (dom.clientWidth - config.min_box_padding)).toFixed(2)),
       1
     ),
     scale_height: Math.max(
-      parseFloat((size_height / (dom.clientHeight - padding)).toFixed(2)),
+      parseFloat((size_height / (dom.clientHeight - config.min_box_padding)).toFixed(2)),
       1
     ),
   };
@@ -133,28 +152,67 @@ const initCells = function () {
     console.warn("templateArr 数据异常");
     return;
   }
+  const changeCell = function (cellsData: CellsOptionType[]) {
+    var _cellsList: TypeObject = {},
+      _cellsImg: {}[] = [];
+    cellsData.forEach(function (item, index) {
+      const [_width, _height] = [parseFloat(width.value), parseFloat(height.value)];
+      _cellsImg.push({});
+      // _cellsList[index.toString()] = {
+      //   width: item.width * _width,
+      //   height: item.height * _height,
+      //   x: item.x * _width,
+      //   y: item.y * _height,
+      // };
+      _cellsList[index.toString()] = {
+        width: item.width * _width,
+        height: item.height * _height,
+        x: item.x * _width,
+        y: item.y * _height,
+      };
+    });
+    cellsList.value = _cellsList;
+    cellsImg.value = _cellsImg;
+  };
   changeCell(template.cells);
 };
 
-const changeCell = function (cellsData: CellsOptionType[]) {
-  var _cellsList: TypeObject = {},
-    _cellsImg: {}[] = [];
-  cellsData.forEach(function (item, index) {
-    const [_width, _height] = [parseFloat(width.value), parseFloat(height.value)];
-    _cellsImg.push({});
-    _cellsList[index.toString()] = {
-      width: item.width * _width,
-      height: item.height * _height,
-      x: item.x * _width,
-      y: item.y * _height,
-    };
-  });
-  cellsList.value = _cellsList;
-  cellsImg.value = _cellsImg;
+const updateImgCache = function (json?: FileOption | null, index?: string) {
+  // 都不传为 清除；传json时写入；传index时修改定位参数
+  if (!json) {
+    imgCache = null;
+    return;
+  }
+  const { id, src, file, width, height } = json;
+  let _width = width;
+  let _height = height;
+  let _scale = "x";
+  if (index != undefined && imgCache) {
+    // 根据 cell 的宽高计算 img 的
+    const xScale = cellsList.value[index].width / width;
+    const yScale = cellsList.value[index].height / height;
+    const scale = Math.max(xScale, yScale);
+    _width = width * scale;
+    _height = height * scale;
+    _scale = scale == xScale ? "y" : "x";
+  }
+  imgCache = {
+    id,
+    src,
+    file,
+    x: 0,
+    y: 0,
+    width: _width,
+    height: _height,
+    scale: _scale as "x" | "y",
+  };
 };
 
-const handleDel = function (item: {}) {
-  console.log(item);
+const handleDel = function (index: number) {
+  // 清除图片
+  if (cellsImg.value[index].id) {
+    cellsImg.value[index] = {};
+  }
 };
 
 const stopHandler = function (e: Event) {
@@ -162,42 +220,109 @@ const stopHandler = function (e: Event) {
   e.preventDefault();
 };
 
-const dragLeave = function (e: DragEvent) {
+// 图库挪入 start
+// 外部调用： 缓存挪入的图片数据
+const setDropCache = function (data: FileOption) {
+  // 设置图片缓存
+  if (!data || !data.id || !data.file || !data.src) {
+    // updateImgCache();
+    throw new Error("拖入文件数据异常");
+  }
+  // 拖入的图片 设置缓存
+  updateImgCache({ ...data });
+  clearIndexForImg = "";
+};
+defineExpose({ setDropCache });
+
+const dragLeave = function (e: DragEvent, index: number) {
   stopHandler(e);
   inDrag.value = "";
 };
 
-const dropAdd = function (e: DragEvent) {
+const dropAdd = function (e: DragEvent, index: number) {
   stopHandler(e);
-  if (inDrag.value == "") return;
+  const _index = index.toString();
+  if (_index == "" || clearIndexForImg == _index) return;
   // 放置在当前位置
-  cellsImg.value[inDrag.value] = dropCache;
+  updateImgCache(imgCache, _index);
+  cellsImg.value[_index] = imgCache;
   inDrag.value = "";
   // 清除来源 img cache
-  if (dropFrom != "") {
-    cellsImg.value[dropFrom] = {};
-    dropFrom = "";
+  if (clearIndexForImg != "") {
+    cellsImg.value[clearIndexForImg] = {};
+    clearIndexForImg = "";
   }
 };
 
-const dragEnter = function (e: DragEvent) {
+const dragStart = function (index: number) {
+  // 缓存： 记录当前的position 拖拽进其他cell
+  const _index = index.toString();
+  updateImgCache({ ...cellsImg.value[_index] }, _index);
+  clearIndexForImg = _index.toString();
+};
+
+const dragEnter = function (e: DragEvent, index: number) {
   stopHandler(e);
-  let target = e.target as HTMLElement;
-  let index = target.dataset.index;
-  if (target.nodeName === "IMG") {
-    // 拖到img上 取上一级 index
-    index = target.parentElement?.dataset.index;
-  }
-  if (index == undefined || inDrag.value == index) return;
-  inDrag.value = index;
+  if (inDrag.value == index.toString()) return;
+  inDrag.value = index.toString();
+};
+// 图库挪入 end
+
+// 图片内部挪动 start
+let dragPositionCache = 0; // 鼠标指针位置缓存
+let moveState = -1; // 是否开启移动事件
+
+const imgMoveStart = function (e: MouseEvent, index: number) {
+  moveState = index;
+  // 缓存： 记录当前的position 拖拽进其他cell
+  dragStart(index);
+  // 初始化移动起点
+  const disX = e.clientX - (e.target as HTMLElement).offsetLeft;
+  const disY = e.clientY - (e.target as HTMLElement).offsetTop;
+  dragPositionCache = cellsImg.value[index].scale == "x" ? disX : disY;
 };
 
-const dragCacheChange = function (e: DragEvent) {
-  let index = (e.target as HTMLElement).parentElement?.dataset.index;
-  if (index == undefined) return;
-  dropCache = { ...cellsImg.value[index] };
-  dropFrom = index;
+const imgMoveEnd = function () {
+  moveState = -1;
 };
+
+const imgMove = function (e: MouseEvent, index: number) {
+  stopHandler(e);
+  if (moveState == -1) return;
+  if (moveState !== index) return;
+  function getValByMax(len: number, max_len: number) {
+    // 边界判断
+    // 根据边距修改值
+    if (len > 0) {
+      return 0;
+    } else if (len < -max_len) {
+      return -max_len;
+    } else {
+      return len;
+    }
+  }
+  switch (cellsImg.value[index].scale) {
+    case "x":
+      if (e.clientX != dragPositionCache) {
+        const disX = e.clientX - (e.target as HTMLElement).offsetLeft;
+        const len = cellsImg.value[index].x + disX - dragPositionCache;
+        const max_len = cellsImg.value[index].width - cellsList.value[index].width;
+        cellsImg.value[index].x = getValByMax(len, max_len);
+        cellsImg.value[index].y = 0;
+      }
+      break;
+    case "y":
+      if (e.clientY != dragPositionCache) {
+        const disY = e.clientY - (e.target as HTMLElement).offsetTop; // 鼠标位移距离
+        const len = cellsImg.value[index].y + disY - dragPositionCache; // 图片位移距离
+        const max_len = cellsImg.value[index].height - cellsList.value[index].height; // 图片位移边距
+        cellsImg.value[index].x = 0;
+        cellsImg.value[index].y = getValByMax(len, max_len);
+      }
+      break;
+  }
+};
+// 图片内部挪动 end
 </script>
 
 <style lang="less" scoped>
@@ -229,10 +354,6 @@ const dragCacheChange = function (e: DragEvent) {
         background-color: var(--main-color-opacity);
         border-color: var(--main-color);
       }
-      img {
-        max-width: 100%;
-        max-height: 100%;
-      }
       .del-btn {
         opacity: 0;
       }
@@ -240,6 +361,15 @@ const dragCacheChange = function (e: DragEvent) {
         opacity: 0.5;
         &:hover {
           opacity: 1;
+        }
+      }
+      &__box {
+        position: relative;
+        overflow: hidden;
+        height: 100%;
+        width: 100%;
+        img {
+          position: absolute;
         }
       }
     }

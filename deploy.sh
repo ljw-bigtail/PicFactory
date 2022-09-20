0dcf8e25dca00763b1c0c2cd7265fa6515fe600c
Leo <leo.lu@orderplus.com>
2022-09-20 10:43:09 +0800

    github page bash

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,7 @@
 #!/usr/bin/env sh
 
-# 发生错误时终止
+# 确保脚本抛出遇到的错误
 set -e
 
-# 构建
-npm run build
-
-# 进入构建文件夹
-cd dist
-
-# 如果你要部署到自定义域名
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# 如果你要部署在 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ljw-bigtail/tools.git master:main
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-cd -
+cd ../ljw-bigtail.github.io
+sh deploy.sh
